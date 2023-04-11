b2801db487085c53e14f81e2533684feb5eb5fe8
Jaga <jagadheesh6@gmail.com>
2023-04-11 22:02:46 +0530

    feat - bind guild, raw, message events

--- src/modules/sockets/socket.client.ts
+++ src/modules/sockets/socket.client.ts
@@ -23,17 +23,57 @@ export class SocketClient {
         GatewayIntentBits.GuildMessageReactions,
       ],
     });
-    this.discordClient.on('ready', () => {
-      emitter.emit('ready');
+
+    /**Ready
+     * @event
+     * @param {Client<true>} client - Discord Client
+     */
+    this.discordClient.on('ready', (client) => {
+      emitter.emit('ready', client as Client<true>);
     });
 
+    /**Raw */
+    this.discordClient.on('raw', (event) => {
+      emitter.emit('raw', event);
+    });
+
+    /**Messages */
     this.discordClient.on('messageCreate', (message) => {
       emitter.emit('messageCreate', message);
     });
 
-    this.discordClient.on('guildUpdate', (oldGuild: Guild, newGuild: Guild) => {
-      emitter.emit('guildUpdate', oldGuild, newGuild);
-      this.eventsHandler.guildUpdate(newGuild.id);
+    this.discordClient.on('messageUpdate', (oldMessage, newMessage) => {
+      emitter.emit('messageUpdate', oldMessage, newMessage);
+    });
+
+    this.discordClient.on('messageDelete', (message) => {
+      emitter.emit('messageDelete', message);
+    });
+
+    this.discordClient.on('messageDeleteBulk', (message, channel) => {
+      emitter.emit('messageDeleteBulk', message, channel);
+    });
+
+    /**Guild */
+    this.discordClient.on('guildCreate', (guild) => {
+      emitter.emit('guildCreate', guild);
+    });
+
+    this.discordClient.on('guildUpdate', (oldGuild, newGuild) => {
+      emitter.emit('guildUpdate', oldGuild as Guild, newGuild as Guild);
+      this.eventsHandler.guildUpdate(newGuild.id); //update cache
+    });
+
+    this.discordClient.on('guildDelete', (guild) => {
+      emitter.emit('guildDelete', guild);
+    });
+
+    this.discordClient.on('guildMemberAdd', (member) => {
+      emitter.emit('guildMemberAdd', member);
+    });
+
+    this.discordClient.on('guildMemberRemove', (member) => {
+      emitter.emit('guildMemberRemove', member);
     });
 
     this.discordClient.login(options.token);
